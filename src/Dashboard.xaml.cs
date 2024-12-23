6d843e664c2d0ace6218f099253a2969e3d673aa
Marco <mshendriks03@gmail.com>
2024-12-24 00:58:26 +0100

    replace MainWindow with Dashboard
    Dashboard will include stats, buttons to go to the action centre etc..

---
+++ src/Dashboard.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using SaveManager.Pages;
+
+namespace SaveManager
+{
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindowContent.Visibility = Visibility.Collapsed;
+                WindowFrame.Navigate(new Setup());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Navigation error: {ex.Message}");
+            }
+        }
+        
+        public void ShowWindowContent()
+        {
+            MainWindowContent.Visibility = Visibility.Visible;
+        }
+    }
+}
