e2dd4867e94d73aaa4adce86d552a2505d7be240
Marco <mshendriks03@gmail.com>
2025-04-05 13:15:33 +0200

    feat: add WriteFormattedTextByType method to terminalUI

--- src/Terminal/TerminalUI.cs
+++ src/Terminal/TerminalUI.cs
@@ -8,6 +8,52 @@ class TerminalUI
         Console.OutputEncoding = Encoding.UTF8;
     }
     
+    public void WriteTextWithColor(string text, ConsoleColor color, bool newLine, bool centered)
+    {
+        if (centered) SetCenterCursorPosition(text.Length);
+        
+        Console.ForegroundColor = color;
+        if (newLine)
+            Console.WriteLine(text);
+        else
+            Console.Write(text);
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+    
+    public void WriteFormattedTextByType(string text, string type, bool newLine, bool centered)
+    {
+        if (centered) SetCenterCursorPosition(text.Length);
+        
+        switch (type)
+        {
+            case "err":
+                SetTypeTextWithColor(text, "err", ConsoleColor.Red, newLine);
+                break;
+            case "suc":
+                SetTypeTextWithColor(text, "suc", ConsoleColor.Green, newLine);
+                break;
+            case "warn":
+                SetTypeTextWithColor(text, "warn", ConsoleColor.Yellow, newLine);
+                break;
+            case "inf":
+                SetTypeTextWithColor(text, "inf", ConsoleColor.Cyan, newLine);
+                break;
+            case "dbg":
+                SetTypeTextWithColor(text, "dbg", ConsoleColor.Magenta, newLine);
+                break;
+            case "dbg-err":
+                SetTypeTextWithColor(text, "dbg-err", ConsoleColor.Magenta, newLine);
+                break;
+            case "dbg-suc":
+                SetTypeTextWithColor(text, "dbg-suc", ConsoleColor.Magenta, newLine);
+                break;
+            case "dbg-inf":
+                SetTypeTextWithColor(text, "dbg-inf", ConsoleColor.Magenta, newLine);
+                break;
+        }
+    }
+    
+// helpers    
     private void SetCenterCursorPosition(int textLength)
     {
         try
@@ -21,15 +67,14 @@ class TerminalUI
         }
     }
     
-    public void WriteTextWithColor(string text, ConsoleColor color, bool newLine, bool centered)
+    private void SetTypeTextWithColor(string text, string type, ConsoleColor color, bool newLine)
     {
-        if (centered) SetCenterCursorPosition(text.Length);
-        
         Console.ForegroundColor = color;
+        Console.Write($"[{type}] ");
+        Console.ForegroundColor = ConsoleColor.Gray;
         if (newLine)
             Console.WriteLine(text);
         else
             Console.Write(text);
-        Console.ForegroundColor = ConsoleColor.Gray;
     }
 }
