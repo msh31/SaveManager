fe9b6ce75a275965acf55bb9f0d142c6205085ca
Marco <180540729+msh31@users.noreply.github.com>
2025-04-07 22:47:03 +0200

    feat: basemanager semi-complete

--- src/Core.cs
+++ src/Core.cs
@@ -1,16 +1,19 @@
 //ReSharper disable InconsistentNaming
 
 using System.Diagnostics;
+using SaveManager.Managers;
 
 class Core
 {
     private readonly TerminalUI _terminalUI;
     //private readonly Logger _logger;
     private readonly CommandProcessor _commandProcessor;
+    private readonly ConfigManager _config;
 
     public Core()
     {
         _terminalUI = new TerminalUI();
+        _config = new ConfigManager();
         //_logger = new Logger(Path.Combine(AppContext.BaseDirectory, "logs", "oops.log"), _terminalUI);
         _commandProcessor = new CommandProcessor(_terminalUI);
     }
@@ -19,7 +22,14 @@ class Core
     {
         Console.Clear();
         _terminalUI.HighlightWordInText($"Welcome to SaveManager, {Environment.UserName}", ConsoleColor.Red, $"{Environment.UserName}", true, true);
-        _terminalUI.HighlightWordInText($"Type 'help' to see available commands!\n", ConsoleColor.Yellow, "'help'", true, true);
+        _terminalUI.HighlightWordInText("Type 'help' to see available commands!\n", ConsoleColor.Yellow, "'help'", true, true);
+        
+        if (_config.Data.FirstRun)
+        {
+            // TODO: add first time initialization (directories, detections (game + accounts), etc.)
+            _config.Data.FirstRun = false;
+            _config.Save();
+        }
         
         // start command loop
         _commandProcessor.Start();
---
+++ src/Managers/ConfigManager.cs
@@ -0,0 +1,52 @@
+﻿namespace SaveManager.Managers;
+
+using System.IO;
+using System.Text.Json;
+
+// holds the data
+public class ConfigData
+{
+    public bool FirstRun { get; set; } = true;
+    public bool UbisoftSupport { get; set; } = true;
+    public bool RockstarSupport { get; set; } = true;
+    public string DetectedUbiAccount { get; set; } = string.Empty;
+    public List<string> DetectedUbiGames { get; set; } = new();
+    public string DetectedRockstarAccount { get; set; } = "unknown";
+    public List<string> DetectedRockstarGames { get; set; } = new();
+}
+
+// handles loading/saving
+public class ConfigManager
+{
+    public ConfigData Data { get; private set; }
+    
+    public ConfigManager()
+    {
+        Data = new ConfigData();
+        Load();
+    }
+    
+    private void Load()
+    {
+        if (File.Exists(Path.Combine(AppContext.BaseDirectory, "config.json")))
+        {
+            var json = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "config.json"));
+            var loadedConfig = JsonSerializer.Deserialize<ConfigData>(json);
+            
+            if (loadedConfig != null)
+            {
+                Data = loadedConfig;
+            }
+        }
+        else
+        {
+            Save();
+        }
+    }
+    
+    public void Save()
+    {
+        var json = JsonSerializer.Serialize(Data, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "config.json"), json);
+    }
+}
