8c1df48d8c0f91de9093a5799268f04c370ecaf4
Marco <180540729+msh31@users.noreply.github.com>
2025-04-09 17:27:49 +0200

    refactor: separate ConfigData from ConfigManager

--- src/Managers/ConfigManager.cs
+++ src/Managers/ConfigManager.cs
@@ -1,21 +1,11 @@
-﻿namespace SaveManager.Managers;
+﻿//ReSharper disable InconsistentNaming
 
+namespace SaveManager.Managers;
+
+using Models;
 using System.IO;
 using System.Text.Json;
 
-// holds the data
-public class ConfigData
-{
-    public bool FirstRun { get; set; } = true;
-    public bool UbisoftSupport { get; set; } = true;
-    public bool RockstarSupport { get; set; } = true;
-    public string DetectedUbiAccount { get; set; } = string.Empty;
-    public List<string> DetectedUbiGames { get; set; } = new();
-    public string DetectedRockstarAccount { get; set; } = "unknown";
-    public List<string> DetectedRockstarGames { get; set; } = new();
-}
-
-// handles loading/saving
 public class ConfigManager
 {
     public ConfigData Data { get; private set; }
---
+++ src/Models/ConfigData.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveManager.Models;
+
+public class ConfigData
+{
+    public bool FirstRun { get; set; } = true;
+    public bool UbisoftSupport { get; set; } = true;
+    public bool RockstarSupport { get; set; } = true;
+    public string DetectedUbiAccount { get; set; } = string.Empty;
+    public List<string> DetectedUbiGames { get; set; } = new();
+    public string DetectedRockstarAccount { get; set; } = "unknown";
+    public List<string> DetectedRockstarGames { get; set; } = new();
+}
