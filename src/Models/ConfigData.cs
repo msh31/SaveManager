a1ccc4405f3fca371268b482967f9bc7e611ce62
Marco <180540729+msh31@users.noreply.github.com>
2025-04-10 17:24:42 +0200

    drop: platform support booleans from config

--- src/Models/ConfigData.cs
+++ src/Models/ConfigData.cs
@@ -3,10 +3,8 @@
 public class ConfigData
 {
     public bool FirstRun { get; set; } = true;
-    public bool UbisoftSupport { get; set; } = true;
-    public bool RockstarSupport { get; set; } = true;
     public string DetectedUbiAccount { get; set; } = string.Empty;
     public List<string> DetectedUbiGames { get; set; } = new();
-    public string DetectedRockstarAccount { get; set; } = "unknown";
+    public string DetectedRockstarAccount { get; set; } = string.Empty;
     public List<string> DetectedRockstarGames { get; set; } = new();
 }
