28e54c1321e523085ebd4c40372b53afe28f940c
Marco <180540729+msh31@users.noreply.github.com>
2025-04-28 17:25:57 +0200

    enhance: use ansiconsole prompt in ListCommand
    and no args

--- src/CommandSystem/Commands/ListCommand.cs
+++ src/CommandSystem/Commands/ListCommand.cs
@@ -7,54 +7,29 @@ class ListCommand : CommandBase
 {
     private readonly UbiManager _ubiManager;
     
-    public ListCommand(UbiManager ubiManager) : base("list", "Shows available saves", "[ubisoft|u|rockstar|r]")
+    public ListCommand(UbiManager ubiManager) : base("list", "Shows available saves", "")
     {
         _ubiManager = ubiManager;
     }
     
     public override async Task ExecuteAsync(string[] args)
     {
-        var fruits = AnsiConsole.Prompt(
-            new MultiSelectionPrompt<string>()
-                .Title("Which [green]games[/] do you want to backup?")
-                .NotRequired() // Not required to have a favorite fruit
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more games)[/]")
-                .InstructionsText(
-                    "[grey](Press [blue]<space>[/] to toggle a game, " + 
-                    "[green]<enter>[/] to accept)[/]")
+        var platform = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Which [green]platform[/] do you want to list saves for?")
+                .PageSize(5)
                 .AddChoices(new[] {
-                    "Apple", "Apricot", "Avocado",
-                    "Banana", "Blackcurrant", "Blueberry",
-                    "Cherry", "Cloudberry", "Coconut",
+                    "Ubisoft",
+                    "Rockstar"
                 }));
 
-// Write the selected fruits to the terminal
-        foreach (string fruit in fruits)
+        if (platform == "Ubisoft")
         {
-            AnsiConsole.WriteLine(fruit);
+            await _ubiManager.ListSaveGamesAsync();
         }
-        
-        // if (args.Length > 0)
-        // {
-        //     var platform = args[0].ToLower();
-        //     
-        //     if (platform is "ubisoft" or "u")
-        //     {
-        //         await _ubiManager.ListSaveGamesAsync();
-        //     }
-        //     else if (platform is "rockstar" or "r")
-        //     {
-        //         _terminalUI.WriteFormattedTextByType("Rockstar list not implemented yet.", "inf", true, false);
-        //     }
-        //     else
-        //     {
-        //         _terminalUI.WriteFormattedTextByType($"Unknown platform: {platform}. Use 'ubisoft' or 'rockstar'", "err", true, false);
-        //     }
-        // }
-        // else
-        // {
-        //     _terminalUI.WriteFormattedTextByType("Please specify platform: 'list ubisoft' or 'list rockstar'", "inf", true, false);
-        // }
+        else
+        {
+            AnsiConsole.MarkupLine("[cyan][[inf]][/] Rockstar list not implemented yet.");
+        }
     }
 }
