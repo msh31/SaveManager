1f4f150a8f7e709dd2b4ec1035989fb6f0ba9e10
Marco <180540729+msh31@users.noreply.github.com>
2025-10-29 21:59:58 +0100

    feat: add enums/structs to savemanager files
    this is the core file

---
+++ src/core/savemanager.cpp
@@ -0,0 +1,6 @@
+#include "savemanager.hpp"
+#include "profile/profile.hpp"
+
+SaveManager::SaveManager() {
+    //get the profile id for the folder name and store it
+}
--- src/core/savemanager.hpp
+++ src/core/savemanager.hpp
@@ -1,3 +1,46 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <chrono>
+
+#include <sentinel/core/logger.h>
+
+enum class PlatformTypes {
+    Ubisoft,
+    Rockstar,
+    Epic,
+};
+
+enum class BackupStatus {
+    None,
+    Partial,
+    Complete
+};
+
+struct GameInfo {
+    std::string gameName;
+    std::string savePath;
+
+    PlatformTypes platform;
+
+    int gameID;
+    int saveCount;
+
+    std::chrono::system_clock::time_point lastModified;
+};
+
+struct BackupRecord {
+    int gameID; //linked to gameinfo's gameid
+    int filesBackedUp;
+    BackupStatus status;
+    std::chrono::system_clock::time_point backupDate;
+    std::string backupPath;
+};
+
+class SaveManager {
+    public:
+        SaveManager();
+        ~SaveManager();
+
+
+};
