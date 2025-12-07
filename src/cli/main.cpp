59fb983abd2cf5864e98ae0dc77b803f53a87989
msh31 <180540729+msh31@users.noreply.github.com>
2025-12-07 23:47:14 +0100

    feat: show my games
    hardcoded af boiii

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include "../core/savemanager.hpp"
+
 int main() {
-    std::cout << "hello from the cli!\n";
+    if (!game_detection::loadGameDatabase("./data/games.json")) {
+        return 1;
+    }
+    
+    auto games = game_detection::scanSteamLibrary("/mnt/games/SteamLibrary", "185654f7-5a7b-44de-baa7-76c4f26fbda4");
+
+    std::cout << "Found " << games.size() << " game(s)\n\n";
+
+    for (const auto& game : games) {
+        std::cout << "Game: " << game.gameName << "\n";
+        std::cout << "  ID: " << game.gameID << "\n";
+        std::cout << "  Saves: " << game.saveCount << " files\n";
+        std::cout << "  Location: " << game.savePath << "\n";
+        std::cout << "\n";
+    }
     return 0;
 }
