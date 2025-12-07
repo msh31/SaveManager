db9e4551f9a05df824aac957429e2a82a0644d0e
msh31 <180540729+msh31@users.noreply.github.com>
2025-12-07 19:51:51 +0100

    attempt 1

--- src/core/detection/profile.cpp
+++ src/core/detection/profile.cpp
@@ -1,12 +1,19 @@
-#include "profile.hpp"
 #include <filesystem>
+#include "profile.hpp"
+
+#ifdef _WIN32
+#define UBI_PROFILE_PATH "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames" 
+#elif defined(__linux__)
+#define UBI_PROFILE_PATH "/home/todo/Ubisoft/Ubisoft Game Launcher/savegames"
+#else
+#error "Unsupported OS"
+#endif
 
 namespace profile {
-    const std::string rootPath = "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames";
     static logger log;
 
     bool userIdExists(const std::string& id) {
-        fs::path fullPath = fs::path(rootPath) / id;
+        fs::path fullPath = fs::path(UBI_PROFILE_PATH) / id;
 
         if (!fs::exists(fullPath) || !fs::is_directory(fullPath)) {
             log.error("Profile '" + id + "' does not exist.");
@@ -19,12 +26,12 @@ namespace profile {
     std::vector<std::string> detectUserIds() {
         std::vector<std::string> profileIds;
 
-        if (!fs::exists(rootPath) || !fs::is_directory(rootPath)) {
+        if (!fs::exists(UBI_PROFILE_PATH) || !fs::is_directory(UBI_PROFILE_PATH)) {
             log.fatal("Ubisoft savegames folder not found!");
             return profileIds;
         }
 
-        for (const auto& entry : fs::directory_iterator(rootPath)) {
+        for (const auto& entry : fs::directory_iterator(UBI_PROFILE_PATH)) {
             if (fs::is_directory(entry.status())) {
                 std::string dirname = entry.path().filename().string();
                 if (dirname.length() == 36) {
