28cb47e6abfd55bfa2ccb2d59d23b75284f58f1c
msh31 <180540729+msh31@users.noreply.github.com>
2025-12-07 23:37:26 +0100

    initial commit

---
+++ src/cli/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "hello from the cli!\n";
+    return 0;
+}
--- src/core/savemanager.cpp
+++
@@ -1,6 +0,0 @@
-#include "savemanager.hpp"
-#include "profile/profile.hpp"
-
-SaveManager::SaveManager() {
-    //get the profile id for the folder name and store it
-}
