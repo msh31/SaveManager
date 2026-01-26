1864a6710d721e1e91904fd1929819368c80f149
Marco <marco@marco007.dev>
2026-01-26 19:31:58 +0100

    move: detection code to list command

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,43 @@
 #include "helpers/colors.hpp"
 #include "helpers/ascii_art.hpp"
 
-// Detection detect;
-
 int main(int argc, char* argv[]) {
-    std::cout << COLOR_RED << printTitle() << COLOR_RESET << "\n\n";
-    auto result = Detection::findSaves();
-
-    if(result.games.empty()) {
-        std::cerr << "No Ubisoft savegames found!\n";
+    if (argc < 2) {
+        std::cout << "Usage: savemanager [list|backup|restore]\n";
         return 1;
     }
 
-    std::cout << COLOR_GREEN << "Found profile: " << COLOR_RESET << result.uuid << "\n";
+    std::cout << COLOR_RED << printTitle() << COLOR_RESET << "\n\n";
 
-    for(const auto& g : result.games) {
-        std::cout << COLOR_RED << "Game Name: " << COLOR_RESET << g.game_name << "\n";
-        std::cout << COLOR_BLUE << "Game ID: " << COLOR_RESET << g.game_id;
-        std::cout << COLOR_GREEN << " Appid: " << COLOR_RESET << g.appid;
-        std::cout << COLOR_YELLOW << " Path: " << COLOR_RESET << g.save_path << "\n\n";
+
+    std::string command = argv[1];
+
+    if (command == "list" || command == "ls") {
+        auto result = Detection::findSaves();
+
+        if(result.games.empty()) {
+            std::cerr << "No Ubisoft savegames found!\n";
+            return 1;
+        }
+
+        std::cout << COLOR_GREEN << "Found profile: " << COLOR_RESET << result.uuid << "\n";
+
+        for(const auto& g : result.games) {
+            std::cout << COLOR_RED << "Game Name: " << COLOR_RESET << g.game_name << "\n";
+            std::cout << COLOR_BLUE << "Game ID: " << COLOR_RESET << g.game_id;
+            std::cout << COLOR_GREEN << " Appid: " << COLOR_RESET << g.appid;
+            std::cout << COLOR_YELLOW << " Path: " << COLOR_RESET << g.save_path << "\n\n";
+        }
     }
-
-    // if (argc < 2) {
-    //     std::cout << "Usage: savemanager\n";
-    //     return 1;
-    // }
+    else if (command == "backup") {
+        std::cout << command << " is a work in progress!" << "\n";
+    }
+    else if (command == "restore") {
+        std::cout << command << " is a work in progress!" << "\n";
+    }
+    else {
+        std::cout << "Unknown command: " << command << "\n";
+        return 1;
+    }
     return 0;
 }
