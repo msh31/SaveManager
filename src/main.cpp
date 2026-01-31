b2cdaaf0c22de3d09167000fd68444aae0cd9bfe
Marco <marco@marco007.dev>
2026-01-31 01:51:38 +0100

    enhance: make save search cross platform

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <cctype>
 #include <iostream>
 
 #include "detection/detection.hpp"
@@ -6,7 +5,7 @@
 #include "command/command.hpp"
 
 int main() {
-    auto result = Detection::findSaves();
+    auto result = Detection::find_saves();
     std::cout << COLOR_RED << print_title() << COLOR_RESET << "\n\n";
 
     if(!config_exists()) {
