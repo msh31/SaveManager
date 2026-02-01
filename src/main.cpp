73166ff21c42e903b55160f2616b8c45be697971
Marco <marco@marco007.dev>
2026-02-01 12:49:46 +0100

    fix: json load fail

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "command/command.hpp"
 
 int main() {
-    auto result = Detection::find_saves();
     #ifndef _WIN32
     std::cout << COLOR_RED << print_title() << COLOR_RESET << "\n\n";
     #endif
@@ -15,6 +14,8 @@ int main() {
         return 1;
     }
 
+
+    auto result = Detection::find_saves();
     if(result.games.empty()) {
         std::cerr << "No Ubisoft savegames found, exiting..\n";
         return 1;
