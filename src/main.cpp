1dd44141e319873a75520b8400c9b36ccd9c2062
Marco <marco@marco007.dev>
2026-02-21 22:45:53 +0100

    wip: add imgui to project with demo window

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include "command/command.hpp"
 #include "ui/menu.hpp"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+
 int main() {
     if(!Config::config_exist()) {
         std::cerr << "Config is missing and could not be generated!\n";
@@ -18,14 +24,89 @@ int main() {
         return 1;
     }
 
-    Menu main_menu("Save Manager");
-    main_menu.add_item("List games", handle_list);
-    main_menu.add_item("Backup", handle_backup);
-    main_menu.add_item("Restore", handle_restore);
-    main_menu.add_exit_item("Quit");
-
-    while(main_menu.run(result)) {
-
+    if(!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW.\n";
+        return 1;
     }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "SaveManager", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // no old OpenGL
+
+    if(window == nullptr) {
+        std::cerr << "Failed to create GLFW window. OpenGL 3.3 support is required!\n";
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    ImGui::CreateContext();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+    do{
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        //ImGui::SetNextWindowViewport(viewport->ID);
+
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar |
+            ImGuiWindowFlags_NoResize |
+            ImGuiWindowFlags_NoMove |
+            ImGuiWindowFlags_NoCollapse |
+            ImGuiWindowFlags_NoBringToFrontOnFocus |
+            ImGuiWindowFlags_NoNavFocus;
+
+        ImGui::Begin("Main Window", nullptr, window_flags);
+
+        float panelWidth = ImGui::GetContentRegionAvail().x * 1.0f;
+        float panelHeight = ImGui::GetContentRegionAvail().y * 1.0f;
+
+        ImGui::SetCursorPosX((ImGui::GetContentRegionAvail().x - panelWidth) * 0.5f);
+        ImGui::SetCursorPosY((ImGui::GetContentRegionAvail().y - panelHeight) * 0.5f);
+
+        ImGui::BeginChild("HostSetupPanel", ImVec2(panelWidth, panelHeight), true, ImGuiChildFlags_AlwaysUseWindowPadding);
+
+        ImGui::Text("SaveManager");
+        ImGui::Separator();
+        ImGui::Dummy(ImVec2(0, 15));
+        ImGui::EndChild();
+
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    while(
+    glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+    glfwWindowShouldClose(window) == 0
+);
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    // Menu main_menu("Save Manager");
+    // main_menu.add_item("List games", handle_list);
+    // main_menu.add_item("Backup", handle_backup);
+    // main_menu.add_item("Restore", handle_restore);
+    // main_menu.add_exit_item("Quit");
+    //
+    // while(main_menu.run(result)) {
+    //
+    // }
     return 0;
 }
