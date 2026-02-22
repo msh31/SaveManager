bac60638f45b7690e38e369676f551387be21860
Marco <marco@marco007.dev>
2026-02-22 21:14:58 +0100

    wip: game cards in general tab

--- src/core/ui/tabs/tabs.cpp
+++ src/core/ui/tabs/tabs.cpp
@@ -2,6 +2,14 @@
 
 void Tabs::render_general_tab(const Fonts& fonts, const Detection::DetectionResult& result) {
     ImGui::Text("test");
+
+    for (const auto& game : result.games) {
+        ImGui::BeginChild(game.game_name.c_str(), ImVec2(200, 150), true);
+        ImGui::Text("%s", game.game_name.c_str());
+        // buttons etc
+        ImGui::EndChild();
+        ImGui::SameLine(); 
+    }
 }
 
 void Tabs::render_log_tab(const Fonts& fonts) {
