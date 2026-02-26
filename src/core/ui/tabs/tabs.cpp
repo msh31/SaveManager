d795c07aef8e0dff2618f2f9dd7fc1d030b34f7b
Marco <marco@marco007.dev>
2026-02-26 16:07:09 +0100

    feat: wire-up backup feature for games
    also added a count to create a grid of the found games

--- src/core/ui/tabs/tabs.cpp
+++ src/core/ui/tabs/tabs.cpp
@@ -1,13 +1,24 @@
 #include "tabs.hpp"
 #include "imgui.h"
+#include "../src/helpers/utils.hpp"
+#include "../src/core/backup/backup.hpp"
+
+#include <filesystem>
 
 void Tabs::render_general_tab(const Fonts& fonts, const Detection::DetectionResult& result, GLuint texture_id) {
     ImGui::PushFont(fonts.header);
     ImGui::Text("Detected Games");
     ImGui::PopFont();
 
+    int count = 0;
     if(!result.games.empty()) {
         for (const auto& game : result.games) {
+            if(count > 5) {
+                ImGui::NewLine();
+                count = 0;
+            }
+
+            count++;
             ImGui::BeginChild(game.game_name.c_str(), ImVec2(250, 300), true);
             ImGui::Text("%s", game.game_name.c_str());
             ImGui::Separator();
@@ -18,10 +29,25 @@ void Tabs::render_general_tab(const Fonts& fonts, const Detection::DetectionResu
 
             if(ImGui::Button("Backup")) {
                 std::cout << "creating backup of: " << game.game_name.c_str() << "!\n";
+
+                fs::path game_backup_dir = game.game_name;
+                if(!fs::exists(backup_dir / game_backup_dir)) {
+                    fs::create_directories(backup_dir / game_backup_dir);
+                }
+
+                fs::path zip_name = backup_dir / game_backup_dir / construct_backup_name(game);
+
+                Backup::create_backup(zip_name, game);
             }
             ImGui::SameLine();
             if(ImGui::Button("Restore")) {
-                std::cout << "restoring backup of: " << game.game_name.c_str() << "!\n";
+                if(fs::is_empty(backup_dir)) {
+                    std::cerr << "No backups were found! Create some first...\n";
+                    return;
+                }
+
+                std::cout << "This feature still needs porting over to the GUI version..\n";
+                // std::cout << "restoring backup of: " << game.game_name.c_str() << "!\n";
             }
 
             ImGui::EndChild();
