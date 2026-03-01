8fa7912e6932b123e72c8838d73305b8bf8f9f6b
marco <marco@marco007.dev>
2026-03-01 13:54:17 +0100

    fix: configure vs script and vcpkg

--- configure-vs.bat
+++ configure-vs.bat
@@ -1,2 +1,6 @@
-cmake -B build-vs -G "Visual Studio 18 2026" -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake
+@echo off
 
+cmake -B build-vs ^
+  -G "Visual Studio 18 2026" ^
+  -A x64 ^
+  -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake"
