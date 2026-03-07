4aac1d8a20dfa49256e7e5c639e42023de9cbe2c
Marco <marco@marco007.dev>
2026-03-07 16:45:30 +0100

    refactor: seperate store detection from main detection class
    not really a class with al lthe static methoods but ion care cuh

---
+++ src/core/detection/rsg/rsg.cpp
@@ -0,0 +1,36 @@
+#include "rsg.hpp"
+#include "core/helpers/translations.hpp"
+#include "core/logger/logger.hpp"
+
+void rsg::find_saves(const fs::path& prefix, std::vector<Game>& out_games) {
+    if(!fs::exists(prefix)) {
+        get_logger().error("Failed to find Rockstar savegames on your system!");
+        return;
+    }
+
+    for(const auto& game : fs::directory_iterator(prefix)) {
+        fs::path game_folder = game.path();
+        std::string folder_name = game_folder.filename().string();
+        fs::path profiles_folder = game_folder / "Profiles";
+
+        if(folder_name == "Launcher" || folder_name == "Social Club") {
+            continue;
+        }
+
+        if(!fs::exists(profiles_folder)) {
+            continue;
+        }
+
+        for(const auto& profile : fs::directory_iterator(profiles_folder)) {
+            fs::path uuid_folder = profile.path();
+
+            Game game;
+            game.type = ROCKSTAR;
+            game.game_name = game_folder.filename().string();
+            game.appid = get_steam_id(game.game_name).value_or("N/A");
+            game.save_path = uuid_folder;
+
+            out_games.push_back(game);
+        }
+    }
+}
