f4e91a1e7286923b4944f5ff14d63355dbb256b2
msh31 <180540729+msh31@users.noreply.github.com>
2026-03-14 23:20:06 +0100

    chore: downgrade VS to VS22
    since cmake3 is required and it wont generate vs26 files.

--- configure-vs.bat
+++ configure-vs.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set VCPKG_ROOT=C:\Program Files\Microsoft Visual Studio\18\Community\VC\vcpkg
+set VCPKG_ROOT=C:\Program Files\Microsoft Visual Studio\17\Community\VC\vcpkg
 
 cmake -B build-vs ^
-  -G "Visual Studio 18 2026" ^
+  -G "Visual Studio 17 2022" ^
   -A x64 ^
   -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
   -DVCPKG_TARGET_TRIPLET=x64-windows-static ^
