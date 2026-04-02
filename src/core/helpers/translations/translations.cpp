aa145c17b5279d42d0a5b32d13d4a94b13e408d1
msh31 <180540729+msh31@users.noreply.github.com>
2026-04-02 15:40:22 +0200

    fix: prevent loading of corrupted translations

--- src/core/helpers/translations/translations.cpp
+++ src/core/helpers/translations/translations.cpp
@@ -22,32 +22,40 @@ void translations::init() {
     std::ifstream ubi_file(paths::ubi_translations().c_str());
     std::ifstream steam_file(paths::steam_appids().c_str());
 
-    if (ubi_file.is_open()) {
-        ubi_data = json::parse(ubi_file);
-        get_logger().info("Loaded ubi_translations JSON with " + std::to_string(ubi_data.size()) + " franchises");
-        for (const auto& [franchise, games] : ubi_data.items()) {
-            for (const auto& [id, name] : games.items()) {
-                auto clean = std::regex_replace(name.get<std::string>(), std::regex(R"(\s*\([^)]*\))"), "");
-                ubi_translations[id] = clean;
+    try {
+        if (ubi_file.is_open() && fs::file_size(paths::ubi_translations()) > 0) {
+            ubi_data = json::parse(ubi_file);
+            get_logger().info("Loaded ubi_translations JSON with " + std::to_string(ubi_data.size()) + " franchises");
+            for (const auto& [franchise, games] : ubi_data.items()) {
+                for (const auto& [id, name] : games.items()) {
+                    auto clean = std::regex_replace(name.get<std::string>(), std::regex(R"(\s*\([^)]*\))"), "");
+                    ubi_translations[id] = clean;
+                }
             }
+        } else {
+            get_logger().error("Failed to open ubi translations file!");
         }
-    } else {
-        get_logger().error("Failed to open ubi translations file!");
+    } catch(json::exception& ex) {
+        get_logger().error("Translation error: " + std::string(ex.what()));
     }
 
-    if (steam_file.is_open()) {
-        steam_data = json::parse(steam_file);
-        get_logger().info("Loaded steamids.json!");
-        for (const auto& [platform, games] : steam_data.items()) {
-            for (const auto& game : games) {
-                std::string name = game["name"].get<std::string>();
-                std::string appid = std::to_string(game["appid"].get<int>());
-                steam_n2i_translation[name] = appid;
-                steam_i2n_translation[appid] = name;
-            }
+    try {
+        if (steam_file.is_open() && fs::file_size(paths::steam_appids()) > 0) {
+            steam_data = json::parse(steam_file);
+            get_logger().info("Loaded steamids.json!");
+            for (const auto& [platform, games] : steam_data.items()) {
+                for (const auto& game : games) {
+                    std::string name = game["name"].get<std::string>();
+                    std::string appid = std::to_string(game["appid"].get<int>());
+                    steam_n2i_translation[name] = appid;
+                    steam_i2n_translation[appid] = name;
+                }
+            } 
+        } else {
+            get_logger().error("Failed to open steamids file!");
         }
-    } else {
-        get_logger().error("Failed to open steamids file!");
+    } catch(json::exception& ex) {
+        get_logger().error("Translation error: " + std::string(ex.what()));
     }
 }
 
