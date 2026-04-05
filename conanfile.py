ebf056baa826b82b6f923435d408e68818764f01
msh31 <180540729+msh31@users.noreply.github.com>
2026-04-05 22:48:56 +0200

    chore: change conanfile.txt to python
    this also fixes libcurl on windows, itll use schannel now which is
    windows native.

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class SaveManager(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("libcurl/8.19.0")
+        self.requires("libzip/1.11.4")
+        self.requires("libssh2/1.11.1")
+        self.requires("imgui/1.92.6")
+        self.requires("glfw/3.4")
+        self.requires("opengl/system")
+        self.requires("nlohmann_json/3.12.0")
+        self.requires("openssl/3.6.1")
+        self.requires("glad/2.0.8")
+        self.requires("stb/cci.20230920")
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.options["libcurl"].with_ssl = "schannel"
+
+    def layout(self):
+        cmake_layout(self)
