f753c7a04a3e53e98a91db4b1b1689218832cc57
msh31 <180540729+msh31@users.noreply.github.com>
2026-04-06 18:42:44 +0200

    chore: update urls for translations

--- src/frontend/views/settings/settings.hpp
+++ src/frontend/views/settings/settings.hpp
@@ -7,8 +7,8 @@ struct SettingsTab {
     void render(const Fonts& fonts, Config& config);
     bool* m_refresh_requested = nullptr;
 
-    std::string_view ubi_translation_url = "https://raw.githubusercontent.com/msh31/smdata/refs/heads/main/ubi_translations.json";
-    std::string_view steam_translation_url = "https://raw.githubusercontent.com/msh31/smdata/refs/heads/main/steamids.json";
+    std::string_view ubi_translation_url = "https://raw.githubusercontent.com/msh31/SaveManager/refs/heads/dev/data/ubi_translations.json";
+    std::string_view steam_translation_url = "https://raw.githubusercontent.com/msh31/SaveManager/refs/heads/dev/data/steamids.json";
 
     std::string blacklist_input;
     std::string new_game_name;
