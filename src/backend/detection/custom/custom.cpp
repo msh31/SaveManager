8d77fe5f601bfc3e07138d5ecd856b6025a9c7dc
marco <marco@marco007.dev>
2026-04-11 17:01:27 +0200

    refactor: extract custom game detection into a lambda

--- src/backend/detection/custom/custom.cpp
+++ src/backend/detection/custom/custom.cpp
@@ -7,13 +7,10 @@ std::expected<std::vector<Game>, DetectionError> CustomDetector::find_saves(cons
     }
     std::vector<Game> games;
 
-    //TODO: extract to a lambda
-    for (const auto& entry : CustomGamesFile::games) {
+    auto process_entry = [&](const auto& entry) {
         fs::path save_path = prefix / entry.save_path;
-         //get_logger().info("Custom check: " + save_path.string());
-
         if (!fs::exists(save_path)) {
-            continue;
+            return;
         }
 
         Game game;
@@ -23,23 +20,10 @@ std::expected<std::vector<Game>, DetectionError> CustomDetector::find_saves(cons
         game.save_path = save_path;
 
         games.push_back(game);
-    }
-    for (const auto& entry : default_games) {
-        fs::path save_path = prefix / entry.save_path;
-        //get_logger().info("Custom check: " + save_path.string());
+    };
 
-        if (!fs::exists(save_path)) {
-            continue;
-        }
-
-        Game game;
-        game.type = PlatformType::CUSTOM;
-        game.game_name = entry.game_name;
-        game.appid = entry.appid;
-        game.save_path = save_path;
-
-        games.push_back(game);
-    }
+    for (const auto& entry : CustomGamesFile::games) process_entry(entry);
+    for (const auto& entry : default_games) process_entry(entry);
 
     return games;
 }
