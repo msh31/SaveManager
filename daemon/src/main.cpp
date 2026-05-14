af7cc3671c3611ed5002aa54d4ae62f7201be7bb
Marco <marco@marco007.dev>
2026-05-14 23:35:25 +0200

    feat(daemon): add watcher for all watch_paths in the config

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -1,10 +1,16 @@
 #include "watcher.hpp"
+
 #include <utils/paths.hpp>
+#include <config/config.hpp>
 
 int main() {
+    Config config;
     Watcher watcher([](const fs::path& path) { SPDLOG_INFO("modified: {}", path.string()); });
-    if(!watcher.add_watch(paths::plugin_dir())) {
-        SPDLOG_ERROR("failed to add watcher for: {}", paths::plugin_dir().string());
+
+    for (auto& entry : config.settings.watch_paths) {
+        if(!watcher.add_watch(entry)) {
+            SPDLOG_ERROR("failed to add watcher for: {}", entry.string());
+        }
     }
     watcher.run();
 }
