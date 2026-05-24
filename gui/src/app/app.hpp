98eed3247451d79fdc430643bdfe1c71165782a9
Marco <marco@marco007.dev>
2026-05-24 15:31:39 +0200

    chore: use lib config in gui (renamed to CConfig as well)

--- gui/src/app/app.hpp
+++ gui/src/app/app.hpp
@@ -1,18 +1,18 @@
 #pragma once
-#include <backend/config/config.hpp>
 #include <backend/ui_manager/ui_manager.hpp>
+#include <config/config.hpp>
 #include <utils/paths.hpp>
 
 #include <frontend/layout/tabbar/tabbar.hpp>
 
 class CApp {
     public:
-    void init( );
-    void render( );
+        void init( );
+        void render( );
 
     private:
-    CConfig m_config;
-    CUIManager m_ui_manager{ std::make_unique<CTabbarShell>( ) };
+        CConfig    m_config;
+        CUIManager m_ui_manager{ std::make_unique<CTabbarShell>( ) };
 
-    bool m_toggle_a = false, m_toggle_b = false, m_toggle_c = true, m_toggle_d = false, m_toggle_e = false;
+        bool m_toggle_a = false, m_toggle_b = false, m_toggle_c = true, m_toggle_d = false, m_toggle_e = false;
 };
