b9be04c8817d90ba8cb2af887f76ce85cbf69ec0
Marco <marco@marco007.dev>
2026-05-24 18:53:14 +0200

    chore: re-add pipeline error in demo pipeline

--- gui/src/backend/text_pipeline/text_pipeline.hpp
+++ gui/src/backend/text_pipeline/text_pipeline.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include "types.hpp"
 
+enum PipelineError {
+    EMPTY_INPUT = 1,    // recoverablej
+    PROCESSING_FAILURE, // show msg
+    INVALID_STATE       // unrecoverable
+};
+
 using Stage = std::function<std::expected<std::string, PipelineError>( std::string )>;
 
 class CTextPipeline {
