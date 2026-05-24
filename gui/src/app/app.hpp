733f22cc2160e3128a619a8cb48563f6edf87ab8
Marco <marco@marco007.dev>
2026-05-24 15:20:17 +0200

    chore: remove sidebar/ribbon layout, disable menubar & statusbar

--- gui/src/app/app.hpp
+++ gui/src/app/app.hpp
@@ -3,9 +3,7 @@
 #include <backend/ui_manager/ui_manager.hpp>
 #include <utils/paths.hpp>
 
-// #include <frontend/layout/tabbar/tabbar.hpp>
-#include <frontend/layout/sidebar/sidebar.hpp>
-// #include <frontend/layout/ribbon/ribbon.hpp>
+#include <frontend/layout/tabbar/tabbar.hpp>
 
 class CApp {
     public:
@@ -14,12 +12,7 @@ class CApp {
 
     private:
     CConfig m_config;
-    // CUIManager m_ui_manager { std::make_unique<CTabbarShell>() };
-    CUIManager m_ui_manager{ std::make_unique<CSidebarShell>( ) };
-    // CUIManager m_ui_manager { std::make_unique<CRibbonShell>() };
+    CUIManager m_ui_manager{ std::make_unique<CTabbarShell>( ) };
 
     bool m_toggle_a = false, m_toggle_b = false, m_toggle_c = true, m_toggle_d = false, m_toggle_e = false;
-
-    CMenuBar m_menubar;
-    CStatusBar m_statusbar;
 };
