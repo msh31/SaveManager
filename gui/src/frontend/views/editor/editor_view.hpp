8058996d07670a2a63e8cf8229fce47c8f728996
Marco <marco@marco007.dev>
2026-05-25 23:06:35 +0200

    feat: re-implement editor tab

--- gui/src/frontend/views/editor/editor_view.hpp
+++ gui/src/frontend/views/editor/editor_view.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "features/save_editor/save_editor.hpp"
 #include <frontend/views/base_view.hpp>
 
 class CEditorView : public CBaseView {
@@ -7,4 +8,8 @@ class CEditorView : public CBaseView {
         void render( ) override;
         void on_enter( ) override {}
         void on_exit( ) override {}
+
+    private:
+        SanAndreas  m_san_andreas;
+        std::string file_path;
 };
