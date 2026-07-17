874603382a5576780f2dced0425a919bd3802801
Marco <marco@marco007.dev>
2026-07-17 23:28:07 +0200

    wip: ea support
    will focus on the codebase first...

--- lib/src/detection/ea/ea.cpp
+++ lib/src/detection/ea/ea.cpp
@@ -5,8 +5,8 @@ std::expected<std::vector<Game>, SMError> CElectronicArtsDetector::find( ) {
     std::vector<fs::path> prefixes = { };
 #ifdef _WIN32
     prefixes.emplace_back( paths::documents_dir( ) );                                // NFS
-    prefixes.emplace_back( paths::home_dir( ) / "AppData" );                         // Battlefield
-    prefixes.emplace_back( paths::home_dir( ) / "ProgramData" / "Electronic Arts" ); // Legacy
+    //prefixes.emplace_back( paths::home_dir( ) / "AppData" );                         // Battlefield
+    //prefixes.emplace_back( paths::home_dir( ) / "ProgramData" / "Electronic Arts" ); // Legacy
 #endif
 
     std::vector<Game> games = { };
@@ -21,26 +21,42 @@ std::expected<std::vector<Game>, SMError> CElectronicArtsDetector::find( ) {
 }
 
 std::vector<Game> CElectronicArtsDetector::scan( fs::path path, const Translations& translations ) {
+    static char NFS_HEADER[4] = { 'N', 'F','S', 'S' };
     if ( !fs::exists( path ) ) return { };
     std::vector<Game> games = { };
 
     for ( const auto& game : fs::directory_iterator( path, fs::directory_options::skip_permission_denied ) ) {
         fs::path game_folder = game.path( );
         std::string folder_name = game_folder.filename( ).string( );
-        fs::path profiles_folder = game_folder / "Profiles";
 
-        for ( const auto& profile : fs::directory_iterator( profiles_folder ) ) {
-            fs::path uuid_folder = profile.path( );
+        // This catches the following:
+        //NFS Unbound, Heat and Payback
+        //if ( folder_name == "Criterion Games" || folder_name == "Ghost Games" ) {
+            //if ( game_folder.filename( ).extension( ) != ".sav" ) continue;
+            //if ( !fs::exists( folder_name ) ) continue;
 
-            Game game;
-            game.type = PlatformType::EA;
-            // game.game_name = translations.get_game_name_rsg( folder_name ).value_or( folder_name );
-            // game.appid = translations.get_steam_id( game.game_name ).value_or( "N/A" );
-            game.save_paths.push_back( uuid_folder );
+            //std::ifstream save( game_folder, std::ifstream::binary );
+            //if ( !save.is_open( ) ) continue;
 
-            SPDLOG_INFO( "[EA] found: {}", game.game_name );
-            games.push_back( game );
-        }
+            //char buffer[4];
+            //save.read( buffer, 4 );
+            //if ( save.gcount( ) != 4 ) continue;
+
+            //if ( !std::ranges::equal( buffer, NFS_HEADER ) ) {
+            //    if ( save.is_open( ) ) save.close( );
+            //    continue;
+            //}
+        //}
+
+        SPDLOG_INFO( "[EA] found: {}", folder_name );
+
+        //Game game;
+        //game.type = PlatformType::EA;
+        //// game.game_name = translations.get_game_name_rsg( folder_name ).value_or( folder_name );
+        //// game.appid = translations.get_steam_id( game.game_name ).value_or( "N/A" );
+        //game.save_paths.push_back( uuid_folder );
+
+        //games.push_back( game );
     }
 
     return games;
