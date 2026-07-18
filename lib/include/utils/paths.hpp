6c47abfd61c111f026dcb22e2b01ed4d9053da52
Marco <marco@marco007.dev>
2026-07-18 23:21:33 +0200

    fix(lib->utils->paths): formatting error for get_known_folder_path method on Windows

--- lib/include/utils/paths.hpp
+++ lib/include/utils/paths.hpp
@@ -63,10 +63,7 @@ namespace paths {
     inline fs::path lutris_dir( ) { return home_dir( ) / "Games"; }
 #endif
 
-    // TODO: make this not ifdef'd
-#if defined( __linux__ ) || defined( __APPLE__ )
     inline fs::path heroic_dir( ) { return home_dir( ) / "Games" / "Heroic"; }
-#endif
 
 #if defined( _WIN32 )
     inline fs::path get_known_folder_path( const KNOWNFOLDERID& folder_id ) {
@@ -77,12 +74,20 @@ namespace paths {
             CoTaskMemFree( path );
             return result;
         } else {
-            auto str = std::format( "Failed to find known folder {}, how did you manage this?", folder_id );
+            //what is logged here is the GUID of a known folder like FOLDERID_Documents or whatever, 
+            // this is a stupid windows type so we're printing this shit raw
+            auto str = std::format(
+                "Failed to find known folder {:08X}-{:04X}-{:04X}-{:02X}{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}, "
+                "how did you manage this?",
+                folder_id.Data1, folder_id.Data2, folder_id.Data3, folder_id.Data4[0], folder_id.Data4[1],
+                folder_id.Data4[2], folder_id.Data4[3], folder_id.Data4[4], folder_id.Data4[5], folder_id.Data4[6],
+                folder_id.Data4[7] );
             throw std::runtime_error( str );
         }
     }
 #endif
 
+    //TODO: dont call this in detectors anymore - its the literal host documetns folder
     inline fs::path documents_dir( ) {
 #if defined( _WIN32 )
         return get_known_folder_path( FOLDERID_Documents );
