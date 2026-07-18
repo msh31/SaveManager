21797e3fe50b90d7042fc935d4c15ff46f09e556
Marco <marco@marco007.dev>
2026-07-19 00:44:48 +0200

    fix(lib->utils) complete resolve_root() implementation
    Windows only

--- lib/src/utils/save_helper/save_helper.cpp
+++ lib/src/utils/save_helper/save_helper.cpp
@@ -1,25 +1,25 @@
 #include "save_helper.hpp"
 
+//Windows only
 fs::path save::resolve_root( SaveRoot sr ) {
+#if not defined( _WIN32 )
+    throw std::runtime_error( "resolve_root was called on a non Windows platform, this should not happen!" );
+#endif
+
     switch ( sr ) {
     case SaveRoot::DOCUMENTS:
-        return paths::documents_dir( ); //
+        return paths::get_known_folder_path( FOLDERID_Documents );
         break;
-#if defined( _WIN32 )
     case SaveRoot::LOCAL_APPDATA:
-        // return paths::get_known_folder_path( FolderID_AppData ) break;
+        return paths::get_known_folder_path( FOLDERID_LocalAppData );
     case SaveRoot::LOCAL_APPDATA_LOW:
-        // return paths::get_known_folder_path( FOLDERID_AppDataProgramData ) break;
+        return paths::get_known_folder_path( FOLDERID_LocalAppDataLow );
         break;
     case SaveRoot::PROGRAM_DATA:
-        return paths::get_known_folder_path( FOLDERID_AppDataProgramData ) break;
+        return paths::get_known_folder_path( FOLDERID_ProgramData );
         break;
     case SaveRoot::SAVED_GAMES:
-        return paths::get_known_folder_path( FOLDERID_SavedGames ) break;
-        break;
-#endif
-    default:
-        return { };
+        return paths::get_known_folder_path( FOLDERID_SavedGames );
         break;
     }
 
