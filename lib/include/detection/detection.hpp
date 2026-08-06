78dce49dd37e7d39d8b2556f7631bb6f5519cca9
Marco <marco@marco007.dev>
2026-08-07 00:33:21 +0200

    chore(lib): remove deprecated god function 'find_saves'
    i forgor

--- lib/include/detection/detection.hpp
+++ lib/include/detection/detection.hpp
@@ -9,11 +9,10 @@
 class IDetector;
 
 namespace Detection {
-    std::vector<Game> find_saves( const Blacklist&, const Translations&, const SteamManifestCache&, UnrealNameCache& );
-
     std::vector<std::unique_ptr<IDetector>> build_detectors(
         const Translations&, const SteamManifestCache&, UnrealNameCache&,
         const std::unordered_map<uint32_t, std::vector<PcgwEntry>>& );
 
     std::vector<Game> de_duplicate( const std::vector<Game>& );
+
 }; // namespace Detection
