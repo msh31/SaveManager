5a7730a6a5929cb8366867fcf01d03260b0fcc18
Marco <marco@marco007.dev>
2026-08-16 17:06:10 +0200

    chore(daemon): make it buildable
    todo...

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -19,29 +19,29 @@ int main( ) {
     SPDLOG_INFO( "daemon started!" );
 
 #if defined( __linux__ )
-    CWatcher watcher(
-        []( const fs::path& path, uint32_t mask ) {
-            if ( !fs::exists( path ) ) SPDLOG_WARN( "{} does not exist", path.string( ) );
-            auto ext = path.extension( ).string( );
+    // CWatcher watcher(
+    //     []( const fs::path& path, uint32_t mask ) {
+    //         if ( !fs::exists( path ) ) SPDLOG_WARN( "{} does not exist", path.string( ) );
+    //         auto ext = path.extension( ).string( );
+    //
+    //         if ( extension_blocklist.contains( ext ) ) return;
+    //         if ( g_extension_blocklist.contains( ext ) ) return;
+    //
+    //         if ( mask & IN_CREATE ) SPDLOG_INFO( "created: {}", path.string( ) );
+    //         else if ( mask & IN_DELETE )
+    //             SPDLOG_INFO( "deleted: {}", path.string( ) );
+    //         else if ( mask & IN_CLOSE_WRITE )
+    //             SPDLOG_INFO( "modified: {}", path.string( ) );
+    //     },
+    //     config );
 
-            if ( extension_blocklist.contains( ext ) ) return;
-            if ( g_extension_blocklist.contains( ext ) ) return;
-
-            if ( mask & IN_CREATE ) SPDLOG_INFO( "created: {}", path.string( ) );
-            else if ( mask & IN_DELETE )
-                SPDLOG_INFO( "deleted: {}", path.string( ) );
-            else if ( mask & IN_CLOSE_WRITE )
-                SPDLOG_INFO( "modified: {}", path.string( ) );
-        },
-        config );
-
-    for ( auto& entry : config.settings.watch_paths ) {
-        if ( !watcher.add_watch( entry ) ) {
-            SPDLOG_ERROR( "failed to add watcher for: {}", entry.string( ) );
-        }
-    }
-    std::thread watcher_thread( &CWatcher::run, &watcher );
-    watcher_thread.detach( );
+    // for ( auto& entry : config.settings.watch_paths ) {
+    //     if ( !watcher.add_watch( entry ) ) {
+    //         SPDLOG_ERROR( "failed to add watcher for: {}", entry.string( ) );
+    //     }
+    // }
+    // std::thread watcher_thread( &CWatcher::run, &watcher );
+    // watcher_thread.detach( );
 #endif
 
     std::thread server_thread( &CServer::run, &m_server );
