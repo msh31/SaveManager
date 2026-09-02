eef0775994cfb9dea91d5224e7440b08ac856fb3
Marco <marco@marco007.dev>
2026-09-02 21:04:34 +0200

    security: update openssl and libcurl to latest versions

--- conanfile.py
+++ conanfile.py
@@ -11,14 +11,14 @@ class SaveManager(ConanFile):
         deps.generate()
 
     def requirements(self):
-        self.requires("libcurl/8.19.0")
+        self.requires("libcurl/8.21.0")
         self.requires("libzip/1.11.4")
         self.requires("libssh2/1.11.1")
         self.requires("imgui/1.92.9b")
         self.requires("glfw/3.4")
         self.requires("opengl/system")
         self.requires("nlohmann_json/3.12.0")
-        self.requires("openssl/3.6.1")
+        self.requires("openssl/3.6.4")
         self.requires("glad/2.0.8")
         self.requires("stb/cci.20230920")
         self.requires("sol2/3.5.0")
