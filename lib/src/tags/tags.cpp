cb8b6803c02f6217b3dca89f8e060e29a369632b
Marco <marco@marco007.dev>
2026-09-04 03:23:26 +0200

    fix(lib->tags): add missing functions from the original features.cpp
    and refactored parts of it to use atomic write to make things mroe
    concise and correct

--- lib/src/tags/tags.cpp
+++ lib/src/tags/tags.cpp
@@ -1,5 +1,6 @@
 #include <tags/tags.hpp>
 #include <utils/paths.hpp>
+#include <utils/utils.hpp>
 #include <logger.hpp>
 
 #include <nlohmann/json.hpp>
@@ -41,21 +42,81 @@ void Tags::migrate_labels_to_tags( ) {
                 j_tags[label.first] = json::array( { label.second } );
             }
 
-            std::ofstream out( tags_file );
-            if ( !out.is_open( ) ) {
-                SPDLOG_ERROR( "Label migration: Failed to write labels.json for: {}", dir.path( ).string( ) );
-                continue;
+            bool write_res = utils::atomic_write( tags_file, j_tags.dump( 4 ) );
+            if ( !write_res ) {
+                SPDLOG_ERROR( "[Tags] Failed to migrate tags!" );
             }
-
-            out << j_tags.dump( 4 );
-            if ( !out.good( ) ) {
-                out.close( );
-                SPDLOG_ERROR( "Failed to save label for: {}", dir.path( ).string( ) );
-                continue;
-            }
-            out.close( );
-
             fs::remove( labels_file );
         }
     }
+}
+
+std::unordered_map<std::string, std::vector<std::string>> Tags::load_tags( const std::string& game ) {
+    std::unordered_map<std::string, std::vector<std::string>> tags;
+    std::string file_name = ( paths::backup_dir( ) / utils::sanitize_filename_path( game ) / "tags.json" ).string( );
+    if ( !fs::exists( file_name ) ) return { };
+
+    std::ifstream in( file_name );
+    if ( !in.is_open( ) ) {
+        SPDLOG_ERROR( "Failed to load tags for {}!", game );
+        return { };
+    }
+
+    json data;
+    try {
+        data = json::parse( in );
+        for ( const auto& entry : data.items( ) ) {
+            tags[entry.key( )] = entry.value( ).get<std::vector<std::string>>( );
+        }
+    } catch ( json::exception& ex ) {
+        SPDLOG_ERROR( "tag parsing error: {}", ex.what( ) );
+        return { };
+    }
+
+    return tags;
+}
+
+std::expected<bool, SMError> Tags::save_tags( const std::string& game, const std::string& filename, const std::vector<std::string>& tags ) {
+    std::string file_name = ( paths::backup_dir( ) / utils::sanitize_filename_path( game ) / "tags.json" ).string( );
+    std::string tmp_name = file_name + ".tmp";
+
+    json data = load_tags( game );
+    data[filename] = tags;
+
+    if ( data[filename].empty( ) ) {
+        data.erase( filename );
+    }
+
+    if ( data.empty( ) ) {
+        fs::remove( file_name );
+        return true;
+    }
+
+    bool write_res = utils::atomic_write( tmp_name, data.dump( 4 ) );
+    if ( !write_res ) {
+        SPDLOG_ERROR( "[Tags] Failed to save tags due to a write error!" );
+        return false;
+    }
+
+    std::error_code ec;
+    fs::rename( tmp_name, file_name, ec );
+    if ( ec ) {
+        SPDLOG_ERROR( "Failed to move tags into place for: {}", game );
+        return false;
+    }
+    return true;
+}
+
+bool Tags::delete_tags( const std::string& game, const std::string& filename ) {
+    std::string file_name = ( paths::backup_dir( ) / utils::sanitize_filename_path( game ) / "tags.json" ).string( );
+
+    json data = load_tags( game );
+    data.erase( filename );
+
+    if ( data.empty( ) ) {
+        fs::remove( file_name );
+        return true;
+    }
+
+    return utils::atomic_write( file_name, data.dump( 4 ) );
 }
