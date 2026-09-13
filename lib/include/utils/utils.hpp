02d241a62c9aa021c1482c9c1cbb3906b77deddd
Marco <marco@marco007.dev>
2026-09-13 13:33:20 +0200

    fix(lib->utils): use MoveFileExW with replace-existing on Windows in atomic_write()

--- lib/include/utils/utils.hpp
+++ lib/include/utils/utils.hpp
@@ -216,6 +216,14 @@ namespace utils { // All functions in this namespace should work across Windows,
 
         file.close( );
 
+        #if defined(_WIN32)
+        BOOL res = MoveFileExW( tmp_path.c_str( ), path.c_str( ), MOVEFILE_REPLACE_EXISTING );
+        if ( !res ) {
+            SPDLOG_ERROR( "[AtomicWrite]: rename error: {}", GetLastError( ) );
+            cleanup( tmp_path );
+            return false;
+        }
+        #else
         std::error_code ec;
         fs::rename( tmp_path, path, ec );
 
@@ -224,7 +232,7 @@ namespace utils { // All functions in this namespace should work across Windows,
             cleanup( tmp_path );
             return false;
         }
-
+        #endif
         return true;
     }
 
