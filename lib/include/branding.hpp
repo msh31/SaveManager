b8a5a01cd4a926311d9cb1fd5e10822366d2299c
Marco <marco@marco007.dev>
2026-09-19 12:20:34 +0200

    chore: set version to 1.11

--- lib/include/branding.hpp
+++ lib/include/branding.hpp
@@ -3,4 +3,4 @@
 // The only file you should need to touch when reusing this framework as a template.
 constexpr const char* APP_NAME = "SaveManager";
 constexpr const char* APP_AUTHOR = "marco007";
-constexpr const char* APP_VERSION = "v1.10.1";
+constexpr const char* APP_VERSION = "v1.11.0";
